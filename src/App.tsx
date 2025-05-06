b403d6c331670cc492f1afc870db1906251a48bc
Mark Carson <markcarson121@gmail.com>
2025-05-06 20:15:08 -0300

    refactored App and moved pages into pages directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,6 @@
-import { useCounterStore } from "./store";
-import { useQuery } from "@tanstack/react-query";
 import { About } from "./pages/About";
-import { Link, Route, Routes } from "react-router-dom";
-
-function Home() {
-  // Zustand store
-  const { count, increment, decrement } = useCounterStore();
-
-  // React Query fetch
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["todos"],
-    queryFn: async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      return response.json();
-    },
-  });
-
-  return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      {/* React Query Data */}
-      <h1>Data (React Query)</h1>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {(error as Error).message}</p>}
-      {data && (
-        <div>
-          <p>Title: {data.title}</p>
-          <p>Completed: {data.completed ? "Yes" : "No"}</p>
-        </div>
-      )}
-      {/* React Router Test */}
-      <Link to="/about">Go to About</Link>
-    </div>
-  );
-}
+import { Home } from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { useCounterStore } from "../../store";
+import { Link } from "react-router-dom";
+
+export function Home() {
+  // Zustand store
+  const { count, increment, decrement } = useCounterStore();
+
+  // React Query fetch
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      return response.json();
+    },
+  });
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      {/* React Query Data */}
+      <h1>Data (React Query)</h1>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {(error as Error).message}</p>}
+      {data && (
+        <div>
+          <p>Title: {data.title}</p>
+          <p>Completed: {data.completed ? "Yes" : "No"}</p>
+        </div>
+      )}
+      {/* React Router Test */}
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
