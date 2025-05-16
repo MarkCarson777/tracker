eeed5dd4f00453d17acd372e609d7acfcc47966b
Mark Carson <markcarson121@gmail.com>
2025-05-16 13:51:03 -0300

    exposed workout properties in WorkoutRecordPage

--- src/pages/WorkoutRecordPage/index.tsx
+++ src/pages/WorkoutRecordPage/index.tsx
@@ -24,7 +24,42 @@ const WorkoutRecordPage: React.FC = () => {
     return <div>Error fetching workout: {error.message}</div>;
   }
 
-  return <div>WorkoutRecordPage</div>;
+  return (
+    <div>
+      <h2>Workout Record</h2>
+      <div>
+        <h3>{workout?.workoutName}</h3>
+        <div>
+          {workout?.exercises.map((exercise, index) => {
+            return (
+              <div key={index}>
+                <h4>{exercise.name}</h4>
+                <p>Type: {exercise.type}</p>
+                {exercise.type === "weights" && (
+                  <div>
+                    {exercise.sets.map((set, setIndex) => {
+                      return (
+                        <div key={setIndex}>
+                          <div className="flex flex-col">
+                            <span>Set {setIndex + 1}</span>
+                            <span>Weight: {set.weight}</span>
+                            <span>Reps: {set.reps}</span>
+                            <span>RPE: {set.failure}</span>
+                            <span>Rest: {set.restTime}</span>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        <p>Notes: {workout?.workoutNotes}</p>
+      </div>
+    </div>
+  );
 };
 
 export { WorkoutRecordPage };
